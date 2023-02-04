839ed4d293d94672b80b9d6b7c4feffea8455b06
Carter Ison <carter@isons.org>
2023-02-04 12:21:22 -0700

    Handle file insertion loops with an error

--- src/Main.hs
+++ src/Main.hs
@@ -23,8 +23,11 @@ module Main where
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Set (Set)
+import qualified Data.Set as S
 import System.FilePath (dropFileName)
 import System.Directory (withCurrentDirectory)
+import Control.Applicative (liftA2)
 
 import Parser
 import Command
@@ -32,9 +35,12 @@ import Command
 main = do
   o <- parseOpts
   r <- process (noRecurse o) $ input o
-  case output o of
-    "-" -> T.putStr r
-    f -> T.writeFile f r
+  case r of
+    (Left e) -> putStrLn e
+    (Right t) ->
+      case output o of
+        "-" -> T.putStr t
+        f -> T.writeFile f t
 
 showHelp :: IO ()
 showHelp = T.putStrLn $ T.unlines helpTxt
@@ -48,21 +54,26 @@ showHelp = T.putStrLn $ T.unlines helpTxt
 withRelativeDir :: FilePath -> IO a -> IO a
 withRelativeDir = withCurrentDirectory . dropFileName
 
-process :: Bool -> FilePath -> IO Text
-process True = processNoRecurse
-process False = processRecurse
+process :: Bool -> FilePath -> IO (Either String Text)
+process True = (pure <$>) . processNoRecurse
+process False = processRecurse S.empty
 
 -- | Process with recursion
-processRecurse :: FilePath -> IO Text
-processRecurse x = withRelativeDir x . f . parse =<< T.readFile x
+processRecurse :: Set FilePath -> FilePath -> IO (Either String Text)
+processRecurse s x = withRelativeDir x . f s . parse =<< T.readFile x
   where
-  f :: [Line] -> IO Text
-  f [] = pure ""
-  f [Literal l] = pure l
-  f (Literal x : xs) = ((x <> "\n") <>) <$> f xs
-  f (Insert x : xs) = do
-    c <- processRecurse x
-    (c <>) <$> f xs
+  f :: Set FilePath -> [Line] -> IO (Either String Text)
+  f _ [] = pure $ Right ""
+  f _ [Literal l] = pure $ Right l
+  f s (Literal x : xs) = (Right (x <> "\n") <<>>) <$> f s xs
+  f s (Insert x : xs)
+    | S.member x s = pure $ Left "Error: circular graph detected"
+    | otherwise = do
+      c <- processRecurse (S.insert x s) x
+      (c <<>>) <$> f s xs
+
+  (<<>>) :: Either e Text -> Either e Text -> Either e Text
+  (<<>>) = liftA2 (<>)
 
 -- | Process without recursion
 processNoRecurse :: FilePath -> IO Text
