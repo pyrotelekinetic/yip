a9576bbceaf763682090345cd02f2244f550b0d4
Carter Ison <carter@isons.org>
2023-09-26 13:09:09 -0700

    Indent flake.nix with 2 spaces

--- flake.nix
+++ flake.nix
@@ -3,48 +3,48 @@
 description = "A very simple preprocessor";
 
 inputs = {
-	nixpkgs = {
-		type = "github";
-		owner = "NixOS";
-		repo = "nixpkgs";
-		ref = "release-22.11";
-	};
+  nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    ref = "release-22.11";
+  };
 };
 
 outputs = { self, nixpkgs }:
-	let
-		pkgs = import nixpkgs { system = "x86_64-linux"; };
-		ghc = pkgs.haskellPackages.ghcWithPackages
-			(a: with a; [
-				megaparsec
-				optparse-applicative
-			]);
-	in {
-	packages.x86_64-linux.default =
-		pkgs.stdenv.mkDerivation {
-			name = "yip";
-			meta = {
-				license = pkgs.lib.licenses.agpl3Plus;
-				description = "A very simple preprocessor";
-			};
-			src = ./src;
-			buildInputs = [
-				ghc
-			];
-			buildPhase = "ghc Main.hs -o yip";
-			installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
-		};
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    ghc = pkgs.haskellPackages.ghcWithPackages
+      (a: with a; [
+        megaparsec
+        optparse-applicative
+      ]);
+  in {
+  packages.x86_64-linux.default =
+    pkgs.stdenv.mkDerivation {
+      name = "yip";
+      meta = {
+        license = pkgs.lib.licenses.agpl3Plus;
+        description = "A very simple preprocessor";
+      };
+      src = ./src;
+      buildInputs = [
+        ghc
+      ];
+      buildPhase = "ghc Main.hs -o yip";
+      installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
+    };
 
-	devShells.x86_64-linux.default = with pkgs;
-		mkShell {
-			packages = [
-				ghcid
-				hlint
-			];
-			buildInputs = [
-				ghc
-			];
-		};
-	};
+  devShells.x86_64-linux.default = with pkgs;
+    mkShell {
+      packages = [
+        ghcid
+        hlint
+      ];
+      buildInputs = [
+        ghc
+      ];
+    };
+  };
 
 }
